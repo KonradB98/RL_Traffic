a0417358bd79bee773abdbc32d745471d7c3e647
Konrad Bajak <konrad.bajak@gmail.com>
2020-09-21 20:00:17 +0200

    Modify QLearning algorithm

--- Enviroment.py
+++ Enviroment.py
@@ -15,22 +15,21 @@ else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
 class TrafficEnv: #Class define structure od SUMO enviroment
-    # Constructor
-    def __init__(self):
+    def __init__(self): #Constructor
         self.detector = "detectors" #multi entry/exit detectors id
-        self.lights = [TwoWayIntersection(id="0")] #List of Lihts(intersections) -- now 1
+        self.intersections = [TwoWayIntersection(id="0")] #List of Lihts(intersections) -- now 1
         self.sim_end = 100 #simulation end time
-        self.obs_space = ["mloop{}".format(i) for i in range(8)] #induction loops detectors id's
+        # self.obs_space = ["mloop{}".format(i) for i in range(8)] #induction loops detectors id's
         self.run_flag = False #running simulation flag -> if True - simulation is running
         self.sim_step = 0 #represents each step of simulation
         self.file_path = os.path.join(os.path.dirname(__file__), "cfg") #Base path to xml project files
         self.roufile = os.path.join(self.file_path, "Intersection.rou.xml") #Name of routes file
         self.cfgfile = os.path.join(self.file_path, "Intersection.sumocfg") #Name of configuration file
         self.infofile = os.path.join(self.file_path, "tripinfo.xml") #name of SUMO output file
-        self.act_space = [a.actions for a in self.lights]
-        self.delay = 1
+        self.act_space = [a.actions for a in self.intersections] #Available actions for each intersections (intersections)
+        self.delay = 1 #Delay between epizodes
 
-    def generateRoutes(self): #method generate routes, write to file "Intersection.rou.xml"
+    def generateRoutes(self): #method generate routes (different scenarios), write to file "Intersection.rou.xml"
         random.seed(1)  # make tests reproducible
         N = 50  # number of vehicles
         pWE = 1. / 5
@@ -69,7 +68,7 @@ class TrafficEnv: #Class define structure od SUMO enviroment
             #         vehNr += 1
             print("</routes>", file=routes)
 
-    def getOptions(self):
+    def getOptions(self): #Method returns simulation options like run with gui or without
         opt_parser = optparse.OptionParser()
         opt_parser.add_option("--nogui", action="store_true",
                               default=True, help="run the commandline version of sumo")
@@ -100,62 +99,53 @@ class TrafficEnv: #Class define structure od SUMO enviroment
         else:
             print("Symulacja nie jest wlaczona!")
 
-
     def getReward(self, o1):
-        # return s1 * s2
         if o1 > 6:
             return -10
         else:
             return 1
 
-    def observation(self):
+    def getEnvState(self):
         veh_amount = traci.multientryexit.getLastStepVehicleNumber(self.detector)
         # veh_speed = traci.multientryexit.getLastStepMeanSpeed(self.detector)
-        light_state = [lgt.state for lgt in self.lights]
+        light_state = [lgt.state for lgt in self.intersections]
         return veh_amount, light_state
-
-
-    def getEnvState(self):
-        state = []
-        for det in self.obs_space:
-            nb = traci.inductionloop.getLastStepVehicleNumber(det)
-            ms = traci.inductionloop.getLastStepMeanSpeed(det)
-            ld = traci.inductionloop.getTimeSinceDetection(det)
-            pom = [nb, ms, ld]
-            state.append(pom)
-            # mv = traci.inductionloop.getLastStepVehicleNumber(det)
-        state_mtrx = np.array(state)
-        light_state = [lgt.state for lgt in self.lights]
-        return state_mtrx, light_state
 
     def step(self, action):
         self.sim_step += 1 #increase simulation step
-
-        # for act, lgt in zip(action, self.lights): #set light (for loop if in the future will be more intersections)
-        #     signal = lgt.setLight(act)
-        #     traci.trafficlight.setPhase(lgt.intersection_id, signal)
-
-        for lgt in self.lights: #set light (for loop if in the future will be more intersections)
+        for lgt in self.intersections: #set light (for loop if in the future will be more intersections)
             signal = lgt.setLight(action)
             traci.trafficlight.setPhase(lgt.intersection_id, signal)
         traci.simulationStep()
-        o1, l = self.observation()
+        o1, l = self.getEnvState()
         r = self.getReward(o1)
         end = self.sim_step > self.sim_end
         if end:
             self.stopSumo()
         return o1, r, end
 
+    def stepik(self, action):
+        self.sim_step += 1 #increase simulation step
+        for act, lgt in zip(action, self.intersections): #set light (for loop if in the future will be more intersections)
+            signal = lgt.setLight(act)
+            traci.trafficlight.setPhase(lgt.intersection_id, signal)
+        traci.simulationStep()
+        o1, l = self.getEnvState()
+        r = self.getReward(o1)
+        end = self.sim_step > self.sim_end
+        if end:
+            self.stopSumo()
+        return o1, r, end
+
+    def reset(self): #Method define steps to reset simulation
+        self.stopSumo() #Stop running simulation
+        time.sleep(self.delay/100.0) #Wait 10 ms (to stop all SUMO processes)
+        self.startSumo() #Start simulation again
+        obs, l = self.getEnvState() #Get enviroment state
+        return obs
+
     def getLightActions(self):
         actions = []
-        for act in self.lights:
+        for act in self.intersections:
             actions = act.getActions()
         return actions
-
-    def reset(self):
-        self.stopSumo()
-        if self.delay > 0:
-            time.sleep(self.delay/100.0)
-        self.startSumo()
-        obs, l = self.observation()
-        return obs
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 def QLearning(Env, Epizodes):
     actions = len(Env.getLightActions()) #Number of available actions (4 for Two way intersection)
+    print(actions)
+    # actions = len(Env.act_space)
     states = Env.sim_end #Number of simulation states
     Q = np.zeros((states, actions)) #Initialize Q table with zeros
 
@@ -23,8 +25,6 @@ def QLearning(Env, Epizodes):
     for episode in range(Epizodes):
         state = Env.reset()
         total_epizode_rewards = 0
-        # step = 0
-        # done = False
         for step in range(Env.sim_end):
             exp_exp_tradeoff = random.uniform(0, 1)
             if exp_exp_tradeoff > epsilon:
@@ -45,14 +45,73 @@ def QLearning(Env, Epizodes):
     # print(cumulative_rewards)
     return cumulative_rewards, epzds
 
+def QAgents(Env, Epizodes):
+    # actions = len(Env.getLightActions()) #Number of available actions (4 for Two way intersection)
+    states = Env.sim_end #Number of simulation states
+    Q_list = []
+    for actions in Env.act_space:
+        actions = len(actions)
+        Q = np.zeros((states, actions))  # Initialize Q table with zeros
+        Q_list.append(Q)
+    # print(Q_list)
+
+    #Hyperparameters
+    alpha = 0.1 #Learning rate
+    gamma = 0.99 #Discount rate
+    epsilon = 1.0
+    max_ep = 1.0
+    min_ep = 0.01
+    exploration_decay_rate = 0.01
+
+    #For charts
+    epzds = []
+    a = []
+
+    cumulative_rewards = []
+    for episode in range(Epizodes):
+        state = Env.reset()
+        total_epizode_rewards = 0
+        for step in range(Env.sim_end):
+            for Q in Q_list:
+                exp_exp_tradeoff = random.uniform(0, 1)
+                if exp_exp_tradeoff > epsilon:
+                    action = np.argmax(Q[state, :])
+                    a.append(action)
+                else:
+                    action = random.randint(0, 3)
+                    a.append(action)
+            new_state, reward, done = Env.stepik(a)
+            for Q, action in zip(Q_list, a):
+                # Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[new_state, :]) - Q[state, action])
+                Q[state, action] = (1 - alpha) * Q[state, action] + alpha * (reward + gamma * np.max(Q[new_state, :]))
+            total_epizode_rewards += reward
+            state = new_state
+            a.clear()
+            if done:
+                break
+        epsilon = min_ep + (max_ep - min_ep) * np.exp(-exploration_decay_rate * episode)
+        cumulative_rewards.append(total_epizode_rewards)
+        epzds.append(episode)
+
+    # # print(cumulative_rewards)
+    return cumulative_rewards, epzds
+
+
+
 if __name__ == "__main__":
     epizodes = []
     rewards_sum = []
     env = TrafficEnv()
-    n_epizodes = 10000
-    rewards_sum, epizodes = QLearning(env, n_epizodes)
+    n_epizodes = 10
+    #------------Modified Algorithm-------------#
+    rewards_sum, epizodes = QAgents(env, n_epizodes)
     plt.plot(epizodes, rewards_sum)
     plt.show()
+
+    # ------------Goood One-------------#
+    # rewards_sum, epizodes = QLearning(env, n_epizodes)
+    # plt.plot(epizodes, rewards_sum)
+    # plt.show()
 
 
 
